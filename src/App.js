24862eccd56d45f68d10c7b47f617e79a1b5f1f8
Vitor Paixao <vitorpaixao@MBP-de-Vitor-2.caliasp.com>
2017-07-14 11:30:36 -0300

    basic css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink } from 'react-router-dom';
+import logo from './Assets/Logo_ML.png';
+import ic_Search from './Assets/ic_Search.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <nav className="navbar navbar-light bg-ml mb-4">
+          <div className="container">
+            <div className="d-flex flex-nowrap">
+              <div>
+                <a className="navbar-brand" href="#">
+                  <img src={logo} alt="logo" />
+                </a>
+              </div>
+
+              <div className="input-group col">
+                <input type="text" className="form-control" placeholder="Nunca dejes de buscar..." />
+                <span className="input-group-btn">
+                  <button className="btn btn-light-grey" type="button">
+                    <img src={ic_Search} alt="logo" />
+                  </button>
+                </span>
+              </div>
+
+            </div>
+          </div>
+          
+        </nav>
+          
+
       </div>
     );
   }
